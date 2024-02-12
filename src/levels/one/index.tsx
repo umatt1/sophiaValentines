4c7dc7378f7cfbaca30d56f9d322cb1bf5377e76
umatt1 <matt.burkhard@gmail.com>
2024-02-11 22:13:44 -0500

    choose when to go to next level

--- src/levels/one/index.tsx
+++ src/levels/one/index.tsx
@@ -4,11 +4,6 @@ import marlowlove from '../../assets/marlowlove.png'
 
 function One(nextLevel: any) {
     const [pets, setPets] = useState(0)
-    useEffect(() => {
-        if (pets>=53) {
-            nextLevel();
-        }
-    }, [pets])
     return <>
     <h2>Level one</h2>
     <h3>Pet Marlow 50 times to continue</h3>
@@ -16,6 +11,7 @@ function One(nextLevel: any) {
     <img className="marlow" src={pets >= 50 ? marlow : marlowlove} alt="Marlow" height='450px' onClick={()=>setPets(pets+1)}/>
     {pets >= 50 &&
         <p>Petting quota achieved</p>}
+        <h2 onClick={() => nextLevel()}>Click to progress to next level</h2>
     </>
 }
 
